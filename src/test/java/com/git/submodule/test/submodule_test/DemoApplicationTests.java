b97da698dfdc4d4bad20dbe88b4809ea78b6ba68
mrotariu-greppysystems <mihai.rotariu@greppysystems.com>
2020-03-16 12:48:13 +0200

    version 1

---
+++ src/test/java/com/git/submodule/test/submodule_test/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.submodule.test.submodule_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
